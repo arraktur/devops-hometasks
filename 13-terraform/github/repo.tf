5452eb2036bbc9c4ca795228301851a3959578dc
Sergey Zagursky <mrak53@mail.ru>
2023-05-17 02:06:39 +0300

    Managed by Terraform

--- 13-terraform/github/repo.tf
+++ 13-terraform/github/repo.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "github_repository_file" "this" {
   for_each = fileset("${path.module}/../","*.tf")
 
@@ -41,48 +40,4 @@ resource "github_repository_file" "git2" {
   content             = file("${path.module}/../github/${each.value}")
   commit_message      = "Managed by Terraform"
   overwrite_on_create = true
-=======
-resource "github_repository_file" "this" {
-  for_each = fileset("${path.module}/../","*.tf")
-
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/${each.value}"
-  content             = file("${path.module}/../${each.value}")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "this2" {
-  for_each = fileset("${path.module}/../","*.tfvars")
-
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/${each.value}"
-  content             = file("${path.module}/../${each.value}")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "git" {
-  for_each = fileset("${path.module}/../github","*.tf")
-
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/github/${each.value}"
-  content             = file("${path.module}/../github/${each.value}")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
-}
-
-resource "github_repository_file" "git2" {
-  for_each = fileset("${path.module}/../github","*.tfvars")
-
-  repository          = "devops-hometasks"
-  branch              = "master"
-  file                = "13-terraform/github/${each.value}"
-  content             = file("${path.module}/../github/${each.value}")
-  commit_message      = "Managed by Terraform"
-  overwrite_on_create = true
->>>>>>> 8aac119 (Managed by Terraform)
 }
