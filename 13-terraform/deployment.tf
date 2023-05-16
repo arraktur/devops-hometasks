44114bbedd074591d8854e36f4ad6326211cf5ed
Sergey Zagursky <mrak53@mail.ru>
2023-05-16 21:14:42 +0300

    Managed by Terraform

---
+++ 13-terraform/deployment.tf
@@ -0,0 +1,41 @@
+resource "kubernetes_deployment_v1" "wcg-depl" {
+  metadata {
+    name = var.app_name
+    }
+
+  spec {
+    replicas = var.deployment_replica_number
+
+    selector {
+      match_labels = {
+        app = var.app_name
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = var.app_name
+        }
+      }
+
+      spec {
+        container {
+          image = var.deployment_container_image
+          name  = var.app_name
+
+          resources {
+            # limits = {
+            #   cpu    = "1"
+            #   memory = "512Mi"
+            # }
+            # requests = {
+            #   cpu    = "1"
+            #   memory = "512Mi"
+            # }
+          }
+        }
+      }
+    }
+  }
+}
