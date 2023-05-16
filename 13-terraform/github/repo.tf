1307558c6278b0517a197aee5795ad328318ba1e
Sergey Zagursky <mrak53@mail.ru>
2023-05-16 21:07:36 +0300

    Managed by Terraform

---
+++ 13-terraform/github/repo.tf
@@ -0,0 +1,21 @@
+resource "github_repository_file" "file" {
+  for_each = fileset("${path.module}/../","*")
+
+  repository          = "devops-hometasks"
+  branch              = "master"
+  file                = "13-terraform/${each.value}"
+  content             = file("${path.module}/../${each.value}")
+  commit_message      = "Managed by Terraform"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "git" {
+  for_each = fileset("${path.module}/../github","*.tf")
+
+  repository          = "devops-hometasks"
+  branch              = "master"
+  file                = "13-terraform/github/${each.value}"
+  content             = file("${path.module}/../github/${each.value}")
+  commit_message      = "Managed by Terraform"
+  overwrite_on_create = true
+}
