332ce154fe413d04f2c87dd9fa8a88d0bacbe180
Sergey Zagursky <mrak53@mail.ru>
2023-05-17 02:06:35 +0300

    Managed by Terraform

--- 13-terraform/providers.tf
+++ 13-terraform/providers.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 terraform {
   required_providers {
     kubernetes = {
@@ -16,23 +15,4 @@ terraform {
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "minikube"
-=======
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.20.0"
-    }
-  }
-
-  backend "kubernetes" {
-    secret_suffix = "state"
-    config_path   = "~/.kube/config"
-  }
-}
-
-provider "kubernetes" {
-  config_path    = "~/.kube/config"
-  config_context = "minikube"
->>>>>>> 8aac119 (Managed by Terraform)
 }
