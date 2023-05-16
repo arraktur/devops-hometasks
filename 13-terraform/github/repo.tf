96ad0e4cf0d573076cb1401208fe872262501ad1
Sergey Zagursky <mrak53@mail.ru>
2023-05-16 21:14:39 +0300

    Managed by Terraform

--- 13-terraform/github/repo.tf
+++ 13-terraform/github/repo.tf
@@ -1,5 +1,16 @@
-resource "github_repository_file" "file" {
-  for_each = fileset("${path.module}/../","*")
+resource "github_repository_file" "this" {
+  for_each = fileset("${path.module}/../","*.tf")
+
+  repository          = "devops-hometasks"
+  branch              = "master"
+  file                = "13-terraform/${each.value}"
+  content             = file("${path.module}/../${each.value}")
+  commit_message      = "Managed by Terraform"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "this2" {
+  for_each = fileset("${path.module}/../","*.tfvars")
 
   repository          = "devops-hometasks"
   branch              = "master"
@@ -11,6 +22,17 @@ resource "github_repository_file" "file" {
 
 resource "github_repository_file" "git" {
   for_each = fileset("${path.module}/../github","*.tf")
+
+  repository          = "devops-hometasks"
+  branch              = "master"
+  file                = "13-terraform/github/${each.value}"
+  content             = file("${path.module}/../github/${each.value}")
+  commit_message      = "Managed by Terraform"
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "git2" {
+  for_each = fileset("${path.module}/../github","*.tfvars")
 
   repository          = "devops-hometasks"
   branch              = "master"
