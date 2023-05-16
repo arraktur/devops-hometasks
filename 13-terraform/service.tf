8c4641e2403e92f6034484fa66c1445ac4edcdbb
Sergey Zagursky <mrak53@mail.ru>
2023-05-17 02:06:44 +0300

    Managed by Terraform

--- 13-terraform/service.tf
+++ 13-terraform/service.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 resource "kubernetes_service_v1" "wcg-svc" {
   metadata {
     name = var.app_name
@@ -14,21 +13,4 @@ resource "kubernetes_service_v1" "wcg-svc" {
     }
     # type = "ClusterIP"
   }
-=======
-resource "kubernetes_service_v1" "wcg-svc" {
-  metadata {
-    name = var.app_name
-  }
-  spec {
-    selector = {
-      app = var.app_name
-    }
-
-    port {
-      port        = var.service_port
-      target_port = var.service_target_port
-    }
-    # type = "ClusterIP"
-  }
->>>>>>> 8aac119 (Managed by Terraform)
 }
